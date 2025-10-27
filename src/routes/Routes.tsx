40f668059073c2f8c827cdca5ee90f2c6151fde1
Borges <jvbona98@gmail.com>
2025-10-27 13:10:10 -0300

    adicionando rota para site

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -3,7 +3,7 @@ import Home from "@/pages/Home";
 import ProjetoTemplate from "@/pages/ProjetoTemplate";
 export default function AppRoutes() {
   return (
-    <Router>
+    <Router basename="/v3">
       <Routes>
         <Route element={<Home />} path="/" />
         <Route path="/projetos/:slug" element={<ProjetoTemplate />} />
