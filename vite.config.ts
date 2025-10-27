fd6a04fa1fdcdd21863502a2e3b2e7176c9f34ba
Borges <jvbona98@gmail.com>
2025-10-27 13:19:56 -0300

    atualizando rota para v3

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/v3/",
   optimizeDeps: {
     include: ["embla-carousel-autoplay"],
   },
